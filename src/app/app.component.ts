85e35166a7fa01ee65d4de98ecec129a3ecdd8ab
Chetan Paliwal <56734563+DevilsSnare@users.noreply.github.com>
2021-10-02 00:48:10 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-list';
+  title = 'todo';
+  message: string = '';
+  list:any[] = [];
+  addTask(item: string) {
+    if(item=="") {
+      this.message="uh-oh! you missed to type the task."
+    } else {
+      if(this.list.find(i=>i.task === item)) {
+        this.message='Task already exists in the list!';
+      } else {
+        this.list.push({id:this.list.length, task: item});
+        this.message="Task added!"
+      }
+    }
+  }
+  deleteTask(id: number) {
+    this.list=this.list.filter(i=>i.id !== id);
+  }
 }
